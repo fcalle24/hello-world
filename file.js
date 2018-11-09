bf712e7e7e13e87458b29c38bf0126be7d5b64fc
fcalle24 <43784676+fcalle24@users.noreply.github.com>
2018-11-08 20:04:44 -0800

    Update file.js

--- file.js
+++ file.js
@@ -6,3 +6,24 @@ function sayHello () {
 var p=document.getElementById("hello");
 p.innerHTML="Hello, World!"
 }
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hello, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+}
+ else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello, ' + storedName;
+}
+
+  myButton.onclick = function() {
+  setUserName();
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
